89e903c3ad22abef04d49163b08a2fbec7d2e0d2
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-05 13:06:30 +0000

    in models/section.rb to_a replaced to []

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,7 +2,7 @@ class Section < ActiveRecord::Base
 	has_many :assets
 	
 	def self.get_title_by_id (section_id)
-		find(section_id).to_a.map{|x| x.title}
+		[find(section_id)].map{|x| x.title}
 	end
 	
 	# def to_param
