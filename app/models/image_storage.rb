1dcc6873cdf5167336a91daab85b55ec9efbc553
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-19 04:31:28 +0000

    Picture Gallery was implemented

--- app/models/image_storage.rb
+++ app/models/image_storage.rb
@@ -1,8 +1,8 @@
 class ImageStorage < ActiveRecord::Base
 	has_attachment :content_type => :image, 
                  :storage => :file_system, 
-                 :max_size => 1.megabyte,
-                 :resize_to => '320x200>',
+                 :max_size => 2.megabyte,
+                 :resize_to => '1000x700>',
                  :thumbnails => { :preview => '100x100>' },
                  :processor => "MiniMagick",
                  :path_prefix => "public/files/image_storage"
