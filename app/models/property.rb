235eaa433c752c69d19659e7493e2b29c44ff656
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-20 14:54:13 +0000

    title is now not required

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,8 +2,6 @@ class Property < ActiveRecord::Base
 		belongs_to :resource, :polymorphic => true
 		has_one :image_storage, :dependent => :destroy
 		
-		validates_presence_of :title
-
 	def title_for_anchor
 		#title.gsub(/ /, '-').gsub(/"|'/,'')
 		title.gsub(/([^ a-zA-Z0-9_.-]+)/n) do
