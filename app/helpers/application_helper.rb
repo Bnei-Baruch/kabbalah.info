48e2bba34202be68833b521796ffdf769713dc7f
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-23 08:35:34 +0000

    bug fix - if no asset has title the TOC box won't be shown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	def has_title?(collection)
+		collection.any?{|asset| !asset.resource.property.title.empty? }
+	end
 end
