c33d524af43a03bdd43d963f8049ae9dd0f5e761
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-23 11:29:41 +0000

    preparations for homepage grey layout

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -135,6 +135,23 @@ class StylesController < ApplicationController
 			:services_hd_fg => "#0698bc",
 			:services_bottom_line => "#8ebdd5",
 		},
+		:grey => {
+			:middle_hd_bg => "#4E4E4E",
+			:middle_hd_fg => "#D7DBDE",
+			:bottom_line => "#73787b",
+			:wide_line => "#cecfd1",							# Wide line under header
+			:h2_font_size => "100%",
+			:category_active => "#FAB597",
+			:general_border => "#a5a7a9",
+			:related_bg => "#f7f7f8",
+			:category_fg => "#77797c",
+			:category_bg => "#FFEFE9",
+			:category_hd_bg => "#909295",
+			:category_hd_fg => "#cecfd1",
+			:services_hd_bg => "#b5d2e2",
+			:services_hd_fg => "#0698bc",
+			:services_bottom_line => "#8ebdd5",
+		},
 	}
 
 	def show
@@ -142,7 +159,7 @@ class StylesController < ApplicationController
 		section = Section.find_by_permalink(params[:section_id])
 		@layout = section.layout
 		@palette_name = section.palette
-		@palette = PALETTE[section.palette.to_sym]
+		@palette = PALETTE[section.palette.to_sym] || PALETTE[:blue]
 		@permitted_assets = section.permitted_assets
     respond_to do |format|
     	if section && File.exists?("#{RAILS_ROOT}/app/views/styles/#{cssfile}")
