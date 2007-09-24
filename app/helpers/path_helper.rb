3ba73078e4d2a6296f549ea282a4254f10ce52e6
ramigg <ramigg@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-24 15:36:07 +0000

    bug in section routes

--- app/helpers/path_helper.rb
+++ app/helpers/path_helper.rb
@@ -1,22 +1,6 @@
 module PathHelper
 	def site_page_url(asset)
-		# categories = []
-		page = asset.resource
-		section = asset.section
-		site_page0_url(:section => section, :id => page )
-		# parent = asset.parent
-		# while parent && parent.resource_type == "Category"
-		# 	categories << parent.resource
-		# 	parent = parent.parent
-		# end
-		# case categories.size
-		# 	when 0
-		# 		section0_url(:section => section, :id => page )
-		# 	when 1
-		# 		section1_url(:section => section, :category0 => categories[0], :id => page )
-		# 	when 2
-		# 		section1_url(:section => section, :category0 => categories[0], :category1 => categories[1], :id => page )
-		# end
+		site_page0_url(:section => asset.section, :id => asset.resource )
 	end
 
 end
