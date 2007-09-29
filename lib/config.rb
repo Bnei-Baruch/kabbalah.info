33365991bbff10e6e8506bc648d6b68b944665ea
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-09-29 11:31:13 +0000

    environments design

--- lib/config.rb
+++ lib/config.rb
@@ -5,151 +5,165 @@ module	Config
 		:strawberry => {
 			# Background of header of a middle (wide) part
 			:middle_hd_bg => "#4E4E4E",						# Background
-			:middle_hd_fg => "#D7DBDE",						# Foreground
-			:bottom_line => "#f5ee4f",						# Bottom line (1px)
+			:middle_hd_fg => "#CCCDCF",						# Foreground
+			:bottom_line => "#4E4E4E",						# Bottom line (1px)
 			:wide_line => "#ff7152",							# Wide line under header
-			:h2_font_size => "100%",							# Size of font
+			:h2_font_size => "85%",							# Size of font
 			# Current (active) category background
 			# Thick line beneath header
 			# Default color for right boxes
 			:category_active => "#ffab90",
 			# Border around boxes
-			:general_border => "#a5a7a9",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#fabca5",
 			# Background of related items & TOC
 			:related_bg => "white",
 			# Categories/pages list
 			:category_fg => "#77797c",		# Foreground
 			:category_bg => "#fff2ec",		# Background
 			:category_hd_bg => "#909295",	# Background of header
-			:category_hd_fg => "#cecfd1",	# Foreground of header
+			:category_hd_fg => "#CCCDCF",	# Foreground of header
+			:category_divider => "#c9e8ea", # line between items
 			# Services
-			:services_hd_bg => "#b5d2e2",	# Background of header
+			:services_hd_bg => "#b3d2e6",	# Background of header
 			:services_hd_fg => "#0698bc",	# Foreground of header
-			:services_bottom_line => "#8ebdd5",	# Bottom line (2px) of header
+			:services_bottom_line => "#8dbdd4",	# Bottom line (2px) of header
 		},
 		:yellow => {
 			# Background of header of a middle (wide) part
 			:middle_hd_bg => "#4E4E4E",						# Background
-			:middle_hd_fg => "#D7DBDE",						# Foreground
+			:middle_hd_fg => "#CCCDCF",						# Foreground
 			:bottom_line => "#f5ee4f",						# Bottom line (1px)
 			:wide_line => "#f4ed4e",							# Wide line under header
-			:h2_font_size => "100%",							# Size of font
+			:h2_font_size => "85%",							# Size of font
 			# Current (active) category background
 			# Thick line beneath header
 			# Default color for right boxes
 			:category_active => "#f9f5b0",
 			# Border around boxes
-			:general_border => "#a5a7a9",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#f4ed4e",
 			# Background of related items & TOC
 			:related_bg => "white",
 			# Categories/pages list
 			:category_fg => "#77797c",		# Foreground
 			:category_bg => "#fbfded",		# Background
 			:category_hd_bg => "#909295",	# Background of header
-			:category_hd_fg => "#cecfd1",	# Foreground of header
+			:category_hd_fg => "#CCCDCF",	# Foreground of header
+			:category_divider => "#f4ed4e", # line between items
 			# Services
-			:services_hd_bg => "#b5d2e2",	# Background of header
+			:services_hd_bg => "#b3d2e6",	# Background of header
 			:services_hd_fg => "#0698bc",	# Foreground of header
-			:services_bottom_line => "#8ebdd5",	# Bottom line (2px) of header
+			:services_bottom_line => "#8dbdd4",	# Bottom line (2px) of header
 		},
 		:olive => {
 			# Background of header of a middle (wide) part
 			:middle_hd_bg => "#4E4E4E",						# Background
-			:middle_hd_fg => "#D7DBDE",						# Foreground
+			:middle_hd_fg => "#CCCDCF",						# Foreground
 			:bottom_line => "#73787b",						# Bottom line (1px)
-			:wide_line => "#bab778",							# Wide line under header
-			:h2_font_size => "100%",							# Size of font
+			:wide_line => "#b8b67b",							# Wide line under header
+			:h2_font_size => "85%",							# Size of font
 			# Current (active) category background
 			# Thick line beneath header
 			# Default color for right boxes
-			:category_active => "#bbb879",
+			:category_active => "#c2bf8c",
 			# Border around boxes
-			:general_border => "#a5a7a9",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#b3b293",
 			# Background of related items & TOC
 			:related_bg => "white",
 			# Categories/pages list
 			:category_fg => "#77797c",		# Foreground
-			:category_bg => "#edefdf",		# Background
+			:category_bg => "#f0f1e3",		# Background
 			:category_hd_bg => "#909295",	# Background of header
-			:category_hd_fg => "#cecfd1",	# Foreground of header
+			:category_hd_fg => "#CCCDCF",	# Foreground of header
+			:category_divider => "#cdca9f", # line between items
 			# Services
-			:services_hd_bg => "#b5d2e2",	# Background of header
+			:services_hd_bg => "#b3d2e6",	# Background of header
 			:services_hd_fg => "#0698bc",	# Foreground of header
-			:services_bottom_line => "#8ebdd5",	# Bottom line (2px) of header
+			:services_bottom_line => "#8dbdd4",	# Bottom line (2px) of header
 		},
 		:blue => {
 			# Background of header of a middle (wide) part
 			:middle_hd_bg => "#4E4E4E",						# Background
-			:middle_hd_fg => "#D7DBDE",						# Foreground
+			:middle_hd_fg => "#CCCDCF",						# Foreground
 			:bottom_line => "#73787b",						# Bottom line (1px)
-			:wide_line => "#afdedf",							# Wide line under header
-			:h2_font_size => "100%",							# Size of font
+			:wide_line => "#aedee0",							# Wide line under header
+			:h2_font_size => "85%",							# Size of font
 			# Current (active) category background
 			:category_active => "#afdedf",
 			# Border around boxes
-			:general_border => "#a5a7a9",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#B1D9DB",
 			# Background of related items & TOC
 			:related_bg => "#f7f7f8",
 			# Categories/pages list
 			:category_fg => "#77797c",		# Foreground
-			:category_bg => "#eff8f8",		# Background
+			:category_bg => "#f1f9fb",		# Background
 			:category_hd_bg => "#909295",	# Background of header
-			:category_hd_fg => "#cecfd1",	# Foreground of header
+			:category_hd_fg => "#CCCDCF",	# Foreground of header
+			:category_divider => "#c9e8ea", # line between items
 			# Services
-			:services_hd_bg => "#b5d2e2",	# Background of header
+			:services_hd_bg => "#b3d2e6",	# Background of header
 			:services_hd_fg => "#0698bc",	# Foreground of header
-			:services_bottom_line => "#8ebdd5",	# Bottom line (2px) of header
+			:services_bottom_line => "#8dbdd4",	# Bottom line (2px) of header
 		},
 		:green => {
-			:middle_hd_bg => "#485458",
-			:middle_hd_fg => "#cecfd1",
+			:middle_hd_bg => "#4E4E4E",
+			:middle_hd_fg => "#CCCDCF",
 			:bottom_line => "#73787b",
 			:wide_line => "#bfdb89",							# Wide line under header
 			:h2_font_size => "85%",
 			:category_active => "#bfdb89",
-			:general_border => "#a5a7a9",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#fabca5",
 			:related_bg => "#f7f7f8",
 			:category_fg => "#77797c",
 			:category_bg => "#f1f7e7",
 			:category_hd_bg => "#909295",
-			:category_hd_fg => "#cecfd1",
-			:services_hd_bg => "#b5d2e2",
+			:category_hd_fg => "#CCCDCF",
+			:category_divider => "#c9e8ea", # line between items
+			:services_hd_bg => "#b3d2e6",
 			:services_hd_fg => "#0698bc",
-			:services_bottom_line => "#8ebdd5",
+			:services_bottom_line => "#8dbdd4",
 		},
 		:orange => {
 			:middle_hd_bg => "#4E4E4E",
-			:middle_hd_fg => "#D7DBDE",
+			:middle_hd_fg => "#CCCDCF",
 			:bottom_line => "#73787b",
-			:wide_line => "#FAB597",							# Wide line under header
-			:h2_font_size => "100%",
-			:category_active => "#FAB597",
-			:general_border => "#a5a7a9",
+			:wide_line => "#f69068",							# Wide line under header
+			:h2_font_size => "85%",
+			:category_active => "#ff9e7c",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#fabca5",
 			:related_bg => "#f7f7f8",
 			:category_fg => "#77797c",
-			:category_bg => "#FFEFE9",
+			:category_bg => "#feefe8",
 			:category_hd_bg => "#909295",
-			:category_hd_fg => "#cecfd1",
-			:services_hd_bg => "#b5d2e2",
+			:category_hd_fg => "#CCCDCF",
+			:category_divider => "#fdd7c4", # line between items
+			:services_hd_bg => "#b3d2e6",
 			:services_hd_fg => "#0698bc",
-			:services_bottom_line => "#8ebdd5",
+			:services_bottom_line => "#8dbdd4",
 		},
 		:grey => {
-			:middle_hd_bg => "#cecfd1",
-			:middle_hd_fg => "#77797c",
+			:middle_hd_bg => "#4E4E4E",
+			:middle_hd_fg => "#CCCDCF",
 			:bottom_line => "#cecfd1",
-			:wide_line => "#a5a7a9",							# Wide line under header
-			:h2_font_size => "100%",
+			:wide_line => "#c2c3c6",							# Wide line under header
+			:h2_font_size => "85%",
 			:category_active => "#cecfd1",
-			:general_border => "#a5a7a9",
+			:general_border => "#c2c3c6",
+			:rbox_border => "#fabca5",
 			:related_bg => "#f7f7f8",
 			:category_fg => "#77797c",
 			:category_bg => "#f4f5f5",
 			:category_hd_bg => "#cecfd1",
-			:category_hd_fg => "#77797c",
-			:services_hd_bg => "#b5d2e2",
+			:category_hd_fg => "#CCCDCF",
+			:category_divider => "#c9e8ea", # line between items
+			:services_hd_bg => "#b3d2e6",
 			:services_hd_fg => "#0698bc",
-			:services_bottom_line => "#8ebdd5",
+			:services_bottom_line => "#8dbdd4",
 		},
 	}
 
