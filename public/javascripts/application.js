10c879ae9545716a57615ab1069bc0e5bb8912fa
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-10-05 02:56:13 +0000

    CSS + links in external window

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,15 @@
 // Replaces 'rel="external"' with 'target="_blank'"
-//function externalLinks() {
-//	if (!document.getElementsByTagName) return;
-//	var anchors = document.getElementsByTagName("a");
-//	for (var i=0; i<anchors.length; i++) {
-//		var anchor = anchors[i];
-//		var relvalue = anchor.getAttribute("rel");
-//
-//		if (anchor.getAttribute("href")) {
-//			var external = /external/;
-//			if (external.test(relvalue)) { anchor.target = "_blank"; }
-//		}
-//	}
-//}
-//window.onload = externalLinks();
+function externalLinks() {
+	if (!document.getElementsByTagName) return;
+	var anchors = document.getElementsByTagName("a");
+	for (var i=0; i<anchors.length; i++) {
+		var anchor = anchors[i];
+		var relvalue = anchor.getAttribute("rel");
+
+		if (anchor.getAttribute("href")) {
+			var external = /external/;
+			if (external.test(relvalue)) { anchor.target = "_blank"; }
+		}
+	}
+}
+window.onload = externalLinks;
